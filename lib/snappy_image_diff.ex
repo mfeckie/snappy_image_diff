b6e93e3ad5a7760c497dc022a66707b322da3078
Martin Feckie <martin.feckie@3plearning.com>
2023-11-18 21:18:49 +0800

    Initial commit

---
+++ lib/snappy_image_diff.ex
@@ -0,0 +1,18 @@
+defmodule SnappyImageDiff do
+  @moduledoc """
+  Documentation for `SnappyImageDiff`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SnappyImageDiff.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/snappy_image_diff_test.exs
@@ -0,0 +1,8 @@
+defmodule SnappyImageDiffTest do
+  use ExUnit.Case
+  doctest SnappyImageDiff
+
+  test "greets the world" do
+    assert SnappyImageDiff.hello() == :world
+  end
+end
