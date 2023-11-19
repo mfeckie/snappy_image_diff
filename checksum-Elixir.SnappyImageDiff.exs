f4184d0a12d0f206139db73fbeee89a6825551ba
Martin Feckie <martin.feckie@3plearning.com>
2023-11-19 14:15:54 +0800

    Add checksum

---
+++ checksum-Elixir.SnappyImageDiff.exs
@@ -0,0 +1,11 @@
+%{
+  "libsnappy_image_diff-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:10ba7d27b0302685be81bc646a81a5a6cbee9b882fd3e360d947448616fab714",
+  "libsnappy_image_diff-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:81e6595a83258dc42372b184db4e6af5411b2535ad1df4a7eb79f839efaa0d8f",
+  "libsnappy_image_diff-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:e377502ba7b766a1a0efb38d350d8b8475d2cc28a13da78e0fb5fb0edcbabc3b",
+  "libsnappy_image_diff-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:acbc8f74213aa593b48d8353ec5f2b151a3cbea6a3555c9b6d3b171b801de9b5",
+  "libsnappy_image_diff-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:559836dc5eddab4f3e012fd559c200a0c57ec722563af5adfcb055deb2c14132",
+  "libsnappy_image_diff-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:c6dd6b67437654b52d1862f54bc031dfc4617a67ccaea79210acb3bfc98c5629",
+  "libsnappy_image_diff-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:2403e507611eec754efb6e42fcea93411d5dd1e9409602e0c38ef98e0ec8c4e8",
+  "snappy_image_diff-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:cd70cd175613479551addf55a0a1e41f784f4d040d21a52bdb1d90a160ea8760",
+  "snappy_image_diff-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:231ebe611a5c59ac7150ebecd814463f368a85b0370975d638e914848361298e",
+}
