f353113562f62777b817b03949cdb3479fe05b73
Martin Feckie <martin.feckie@3plearning.com>
2023-11-19 12:55:12 +0800

    Switch to image-compare

--- lib/snappy_image_diff.ex
+++ lib/snappy_image_diff.ex
@@ -1,7 +1,15 @@
 defmodule SnappyImageDiff do
   use Rustler, otp_app: :snappy_image_diff, crate: :diff
 
-  def diff(_before, _after), do: :erlang.nif_error(:nif_not_loaded)
+  def diff(before_path, after_path) do
+    expanded = Path.expand(before_path)
+    filename = Path.basename(expanded, ".png")
+    directory = Path.dirname(expanded)
 
-  def diff_and_save(_before, _after, _output_path), do: :erlang.nif_error(:nif_not_loaded)
+    output_path = Path.join([directory, "#{filename}-diff"])
+
+    diff(before_path, after_path, output_path)
+  end
+
+  def diff(_before, _after, _output_path), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/diff/src/lib.rs
+++ native/diff/src/lib.rs
@@ -1,8 +1,8 @@
 use image::io::Reader as ImageReader;
 use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
+use image_compare::Algorithm;
 use rustler::types::atom;
 use rustler::{Encoder, Env, NifResult, Term};
-use std::io::Cursor;
 
 rustler::atoms! {
     decode_error,
@@ -50,35 +50,31 @@ pub fn diff_and_save<'a>(
 }
 
 #[rustler::nif]
-pub fn diff<'a>(env: Env<'a>, before_path: &str, after_path: &str) -> NifResult<Term<'a>> {
-    let before = match open_image(before_path) {
-        Ok(image) => image,
-        Err((atom, message)) => return Ok((atom, message).encode(env)),
+pub fn diff<'a>(env: Env<'a>, before_path: &str, after_path: &str, output_path: &str) -> NifResult<Term<'a>> {
+    let before = match image::open(before_path) {
+        Ok(image) => image.to_rgb8(),
+        Err(_) => return Ok((atom::error(), not_found()).encode(env)),
     };
 
-    let after = match open_image(after_path) {
-        Ok(image) => image,
-        Err((atom, message)) => return Ok((atom, message).encode(env)),
+    let after = match image::open(after_path) {
+        Ok(image) => image.to_rgb8(),
+        Err(_) => return Ok((atom::error(), not_found()).encode(env)),
     };
 
-    let (after_width, after_height) = after.dimensions();
-
-    let (before_width, before_height) = before.dimensions();
-
-    if (after_width != before_width) || (after_height != before_height) {
-        return Ok((atom::error(), dimension_mismatch()).encode(env));
-    }
-
-    let result = build_diff(&before, &after);
-
-    if result != before {
-        let mut bytes: Vec<u8> = Vec::new();
-        match result.write_to(&mut Cursor::new(&mut bytes), image::ImageOutputFormat::Png) {
-            Ok(_) => return Ok((atom::error(), different(), bytes).encode(env)),
-            Err(_) => return Ok((atom::error(), write_failure()).encode(env)),
+    match image_compare::rgb_similarity_structure(&Algorithm::RootMeanSquared, &before, &after) {
+        Ok(result) => {
+            if result.score == 1.0 {
+                return Ok((atom::ok(), images_match()).encode(env));
+            } else {
+                let output_file = format!("{}.png", output_path);
+                
+                match result.image.to_color_map().save(&output_file) {
+                    Ok(_) => return Ok((atom::error(), different(), output_file).encode(env)),
+                    Err(_) => return Ok((atom::error(), write_failure()).encode(env)),
+                }
+            }
         }
-    } else {
-        return Ok((atom::ok(), images_match()).encode(env));
+        Err(_) => return Ok((atom::error(), dimension_mismatch()).encode(env)),
     }
 }
 
